3f18260b1fcf2c95d007f68f4becf0c0e8029ed0
global-github-at-238090515572 <>
2021-11-02 16:24:53 +0800

    init repo

---
+++ app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from aws_cdk import core
+
+from workshop.stack.production_stack import ProductionStack
+from workshop.stack.cicdpipeline_stack import CICDPipelineStack
+from workshop.stack.webapplication_stack import WebApplicationStack
+
+UserName = ""
+EmailAddress = ""
+Region = ""
+
+app = core.App()
+Production = ProductionStack(app, 
+    "Production-" + UserName.lower(),
+    UserName = UserName.lower(),
+    EmailAddress = EmailAddress,
+    env={'region': Region}
+)
+
+
+WebApplication = WebApplicationStack(app, 
+    "WebApplication-" + UserName.lower(),
+    UserName = UserName.lower(),
+    EmailAddress = EmailAddress,
+    Vpc=Production.getVpc(),
+    StateMachine = Production.getStateMachine(),
+    env={'region': Region}
+)
+
+CICDPipeline = CICDPipelineStack(app, 
+    "CICDPipeline-" + UserName.lower(),
+    UserName = UserName.lower(),
+    EmailAddress = EmailAddress,
+    BatchRepo = Production.getEcrRepo(),
+    WebRepo = WebApplication.getEcrRepo(),
+    WebService = WebApplication.getService(),
+    env={'region': Region}
+)
+
+
+app.synth()
